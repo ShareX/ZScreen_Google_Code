39650fb99825e5ec7e5931f46cd456ba7d4db9d4
Jaex <Jaex@users.noreply.github.com>
2009-03-09 13:23:47 +0000

    Making ColorPicker control and ColorDialog

--- MyColorsTest/MyColorsTest/Form1.Designer.cs
+++ MyColorsTest/MyColorsTest/Form1.Designer.cs
@@ -111,7 +111,7 @@ namespace MyColorsTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Silver;
+            this.BackColor = System.Drawing.Color.Gainsboro;
             this.ClientSize = new System.Drawing.Size(301, 451);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
