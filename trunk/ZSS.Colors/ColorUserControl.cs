295ca9b0d9a4e192a05e71c3c03d1b239d496630
flexy123 <flexy123@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-03-09 21:41:56 +0000

    Color calculations more accurate now

--- trunk/ZSS.Colors/ColorUserControl.cs
+++ trunk/ZSS.Colors/ColorUserControl.cs
@@ -119,7 +119,6 @@ namespace ZSS.Colors
             this.ClientSizeChanged += new System.EventHandler(this.EventClientSizeChanged);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.EventMouseDown);
             this.MouseEnter += new EventHandler(this.EventMouseEnter);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.EventMouseMove);
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.EventMouseUp);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.EventPaint);
 
@@ -161,11 +160,6 @@ namespace ZSS.Colors
             {
                 this.Cursor = new Cursor(Helpers.GetImageResource("ZSS.Colors.Cursor.cur"));
             }
-        }
-
-        private void EventMouseMove(object sender, MouseEventArgs e)
-        {
-
         }
 
         private void EventMouseUp(object sender, MouseEventArgs e)
@@ -240,28 +234,28 @@ namespace ZSS.Colors
             switch (DrawStyle)
             {
                 case DrawStyle.Hue:
-                    lastPos.X = Round(width * SetColor.HSB.Saturation);
-                    lastPos.Y = Round(height * (1.0 - SetColor.HSB.Brightness));
+                    lastPos.X = Round((width - 1) * SetColor.HSB.Saturation);
+                    lastPos.Y = Round((height - 1) * (1.0 - SetColor.HSB.Brightness));
                     break;
                 case DrawStyle.Saturation:
-                    lastPos.X = Round(width * SetColor.HSB.Hue);
-                    lastPos.Y = Round(height * (1.0 - SetColor.HSB.Brightness));
+                    lastPos.X = Round((width - 1) * SetColor.HSB.Hue);
+                    lastPos.Y = Round((height - 1) * (1.0 - SetColor.HSB.Brightness));
                     break;
                 case DrawStyle.Brightness:
-                    lastPos.X = Round(width * SetColor.HSB.Hue);
-                    lastPos.Y = Round(height * (1.0 - SetColor.HSB.Saturation));
+                    lastPos.X = Round((width - 1) * SetColor.HSB.Hue);
+                    lastPos.Y = Round((height - 1) * (1.0 - SetColor.HSB.Saturation));
                     break;
                 case DrawStyle.Red:
-                    lastPos.X = Round(width * (double)SetColor.RGB.Blue / 255);
-                    lastPos.Y = Round(height * (1.0 - (double)SetColor.RGB.Green / 255));
+                    lastPos.X = Round((width - 1) * (double)SetColor.RGB.Blue / 255);
+                    lastPos.Y = Round((height - 1) * (1.0 - (double)SetColor.RGB.Green / 255));
                     break;
                 case DrawStyle.Green:
-                    lastPos.X = Round(width * (double)SetColor.RGB.Blue / 255);
-                    lastPos.Y = Round(height * (1.0 - (double)SetColor.RGB.Red / 255));
+                    lastPos.X = Round((width - 1) * (double)SetColor.RGB.Blue / 255);
+                    lastPos.Y = Round((height - 1) * (1.0 - (double)SetColor.RGB.Red / 255));
                     break;
                 case DrawStyle.Blue:
-                    lastPos.X = Round(width * (double)SetColor.RGB.Red / 255);
-                    lastPos.Y = Round(height * (1.0 - (double)SetColor.RGB.Green / 255));
+                    lastPos.X = Round((width - 1) * (double)SetColor.RGB.Red / 255);
+                    lastPos.Y = Round((height - 1) * (1.0 - (double)SetColor.RGB.Green / 255));
                     break;
             }
             lastPos = GetPoint(lastPos);
@@ -272,33 +266,33 @@ namespace ZSS.Colors
             switch (DrawStyle)
             {
                 case DrawStyle.Hue:
-                    mSetColor.HSB.Saturation = (double)lastPos.X / width;
-                    mSetColor.HSB.Brightness = 1.0 - (double)lastPos.Y / height;
+                    mSetColor.HSB.Saturation = (double)lastPos.X / (width - 1);
+                    mSetColor.HSB.Brightness = 1.0 - (double)lastPos.Y / (height - 1);
                     mSetColor.HSBUpdate();
                     break;
                 case DrawStyle.Saturation:
-                    mSetColor.HSB.Hue = (double)lastPos.X / width;
-                    mSetColor.HSB.Brightness = 1.0 - (double)lastPos.Y / height;
+                    mSetColor.HSB.Hue = (double)lastPos.X / (width - 1);
+                    mSetColor.HSB.Brightness = 1.0 - (double)lastPos.Y / (height - 1);
                     mSetColor.HSBUpdate();
                     break;
                 case DrawStyle.Brightness:
-                    mSetColor.HSB.Hue = (double)lastPos.X / width;
-                    mSetColor.HSB.Saturation = 1.0 - (double)lastPos.Y / height;
+                    mSetColor.HSB.Hue = (double)lastPos.X / (width - 1);
+                    mSetColor.HSB.Saturation = 1.0 - (double)lastPos.Y / (height - 1);
                     mSetColor.HSBUpdate();
                     break;
                 case DrawStyle.Red:
-                    mSetColor.RGB.Blue = Round(255 * (double)lastPos.X / width);
-                    mSetColor.RGB.Green = Round(255 * (1.0 - (double)lastPos.Y / height));
+                    mSetColor.RGB.Blue = Round(255 * (double)lastPos.X / (width - 1));
+                    mSetColor.RGB.Green = Round(255 * (1.0 - (double)lastPos.Y / (height - 1)));
                     mSetColor.RGBUpdate();
                     break;
                 case DrawStyle.Green:
-                    mSetColor.RGB.Blue = Round(255 * (double)lastPos.X / width);
-                    mSetColor.RGB.Red = Round(255 * (1.0 - (double)lastPos.Y / height));
+                    mSetColor.RGB.Blue = Round(255 * (double)lastPos.X / (width - 1));
+                    mSetColor.RGB.Red = Round(255 * (1.0 - (double)lastPos.Y / (height - 1)));
                     mSetColor.RGBUpdate();
                     break;
                 case DrawStyle.Blue:
-                    mSetColor.RGB.Red = Round(255 * (double)lastPos.X / width);
-                    mSetColor.RGB.Green = Round(255 * (1.0 - (double)lastPos.Y / height));
+                    mSetColor.RGB.Red = Round(255 * (double)lastPos.X / (width - 1));
+                    mSetColor.RGB.Green = Round(255 * (1.0 - (double)lastPos.Y / (height - 1)));
                     mSetColor.RGBUpdate();
                     break;
             }
@@ -309,22 +303,22 @@ namespace ZSS.Colors
             switch (DrawStyle)
             {
                 case DrawStyle.Hue:
-                    lastPos.Y = height - Round(height * SetColor.HSB.Hue);
+                    lastPos.Y = (height - 1) - Round(height * SetColor.HSB.Hue);
                     break;
                 case DrawStyle.Saturation:
-                    lastPos.Y = height - Round(height * SetColor.HSB.Saturation);
+                    lastPos.Y = (height - 1) - Round(height * SetColor.HSB.Saturation);
                     break;
                 case DrawStyle.Brightness:
-                    lastPos.Y = height - Round(height * SetColor.HSB.Brightness);
+                    lastPos.Y = (height - 1) - Round(height * SetColor.HSB.Brightness);
                     break;
                 case DrawStyle.Red:
-                    lastPos.Y = height - Round(height * (double)SetColor.RGB.Red / 255);
+                    lastPos.Y = (height - 1) - Round(height * (double)SetColor.RGB.Red / 255);
                     break;
                 case DrawStyle.Green:
-                    lastPos.Y = height - Round(height * (double)SetColor.RGB.Green / 255);
+                    lastPos.Y = (height - 1) - Round(height * (double)SetColor.RGB.Green / 255);
                     break;
                 case DrawStyle.Blue:
-                    lastPos.Y = height - Round(height * (double)SetColor.RGB.Blue / 255);
+                    lastPos.Y = (height - 1) - Round(height * (double)SetColor.RGB.Blue / 255);
                     break;
             }
             lastPos = GetPoint(lastPos);
@@ -335,27 +329,27 @@ namespace ZSS.Colors
             switch (DrawStyle)
             {
                 case DrawStyle.Hue:
-                    mSetColor.HSB.Hue = 1.0 - (double)lastPos.Y / height;
+                    mSetColor.HSB.Hue = 1.0 - (double)lastPos.Y / (height - 1);
                     mSetColor.HSBUpdate();
                     break;
                 case DrawStyle.Saturation:
-                    mSetColor.HSB.Saturation = 1.0 - (double)lastPos.Y / height;
+                    mSetColor.HSB.Saturation = 1.0 - (double)lastPos.Y / (height - 1);
                     mSetColor.HSBUpdate();
                     break;
                 case DrawStyle.Brightness:
-                    mSetColor.HSB.Brightness = 1.0 - (double)lastPos.Y / height;
+                    mSetColor.HSB.Brightness = 1.0 - (double)lastPos.Y / (height - 1);
                     mSetColor.HSBUpdate();
                     break;
                 case DrawStyle.Red:
-                    mSetColor.RGB.Red = 255 - Round(255 * (double)lastPos.Y / height);
+                    mSetColor.RGB.Red = 255 - Round(255 * (double)lastPos.Y / (height - 1));
                     mSetColor.RGBUpdate();
                     break;
                 case DrawStyle.Green:
-                    mSetColor.RGB.Green = 255 - Round(255 * (double)lastPos.Y / height);
+                    mSetColor.RGB.Green = 255 - Round(255 * (double)lastPos.Y / (height - 1));
                     mSetColor.RGBUpdate();
                     break;
                 case DrawStyle.Blue:
-                    mSetColor.RGB.Blue = 255 - Round(255 * (double)lastPos.Y / height);
+                    mSetColor.RGB.Blue = 255 - Round(255 * (double)lastPos.Y / (height - 1));
                     mSetColor.RGBUpdate();
                     break;
             }
