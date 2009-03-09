c4cd6a4c43931aae737479bed91e315e28fd8a3e
McoreD <mcored@gmail.com>
2009-03-09 07:21:45 +0000

    Output folder moved up one level

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -53,4 +53,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.1")]
+[assembly: AssemblyFileVersion("2.1.0.2")]
