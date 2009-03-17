2cd2236bf27dbd8ab6b0452a15df5661339b91d4
flexy123 <flexy123@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-03-17 03:42:55 +0000

    DLL folders removed. Fixed active help check in start.

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,8 +52,15 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.1.136")]
+[assembly: AssemblyVersion("2.3.1.138")]
 [assembly: AssemblyFileVersion("2.3.1.0")]
+
+
+
+
+
+
+
 
 
 
