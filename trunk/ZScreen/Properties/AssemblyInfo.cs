1f45f55a6bccf1d836abdf2dcdcceba64f99379c
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-03-17 08:03:31 +0000

    Added (strangely) missing ZSS.Colors project in sln

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.1.142")]
+[assembly: AssemblyVersion("2.3.1.143")]
 [assembly: AssemblyFileVersion("2.3.1.0")]
