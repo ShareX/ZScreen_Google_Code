b10bbd1ec839dd7efd1ec21981fee5f0ade8f63f
McoreD <mcored@gmail.com>
2009-03-19 12:56:37 +0000

    Experimental code to fix random URL appending issue

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.4.0.149")]
+[assembly: AssemblyVersion("2.4.0.165")]
 [assembly: AssemblyFileVersion("2.4.0.0")]
