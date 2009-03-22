21809ad5f318377b829f5e4fc21f3dc418aef6be
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-03-22 00:37:54 +0000

    Revamped Update Checker

---
+++ trunk/ZSS.UpdateChecker/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZSS.UpdateCheckerLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
