b4f917862106311a3516d885e7dab68cec009f8d
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-03-29 12:50:10 +0000

    2.6.4.0 Added options to customize History List format in Options : History tab

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.4.0.245")]
-[assembly: AssemblyFileVersion("2.6.3.0")]
+[assembly: AssemblyVersion("2.4.0.257")]
+[assembly: AssemblyFileVersion("2.6.4.0")]
