98214c0776648dcbd6100e99e398d7f971d87422
McoreD <mcored@gmail.com>
2009-03-31 12:30:11 +0000

    2.6.6.0 Crop Shot Crosshair color is now customizable 2.6.5.0 Drag n Drop Window is now movable and the location is remembered

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.6.4.259")]
-[assembly: AssemblyFileVersion("2.6.4.0")]
+[assembly: AssemblyVersion("2.6.4.267")]
+[assembly: AssemblyFileVersion("2.6.6.0")]
