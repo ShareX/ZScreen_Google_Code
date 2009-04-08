9fbf33b06fc08fcec718a5a727c9a82e74f13429
flexy123 <flexy123@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-04-08 17:12:50 +0000

    ZScreen method organized. Debug tab have start/pause buttons. Added event instead use timer for Debug class.

--- trunk/ZScreen/Global/EventHandlers.cs
+++ trunk/ZScreen/Global/EventHandlers.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 namespace ZSS
 {
+    public delegate void StringEventHandler(object sender, string str);
     public delegate void StringsEventHandler(object sender, string[] strings);
     public delegate void IntegersEventHandler(object sender, int[] integers);
     public delegate void JobsEventHandler(object sender, Tasks.MainAppTask.Jobs jobs);
