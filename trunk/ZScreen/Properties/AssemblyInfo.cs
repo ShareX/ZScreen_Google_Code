5817396223a4d88bc19d86b5bfb7dd87671064cd
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-04-10 14:50:47 +0000

    Release Configuration setting changed to allow Unsafe Code

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.6.4.346")]
-[assembly: AssemblyFileVersion("2.6.17.1")]
+[assembly: AssemblyVersion("2.6.4.348")]
+[assembly: AssemblyFileVersion("2.6.18.0")]
