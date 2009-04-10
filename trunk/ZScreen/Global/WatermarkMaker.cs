101c35ff8fc5ec2cf896696e789c86716d19584b
flexy123 <flexy123@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-04-10 13:19:51 +0000

    Added watermark image reflection

--- trunk/ZScreen/Global/WatermarkMaker.cs
+++ trunk/ZScreen/Global/WatermarkMaker.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 using System.IO;
+using System.Drawing.Imaging;
 
 namespace ZSS
 {
@@ -72,6 +73,8 @@ namespace ZSS
                             {
                                 g.DrawRectangle(new Pen(Color.Black), new Rectangle(imgRect.X - 1, imgRect.Y - 1, imgRect.Width, imgRect.Height));
                             }
+                            Bitmap img3 = Transparent((Bitmap)img2);
+                            g.DrawImage(img3, new Rectangle(imgRect.X, imgRect.Y + imgRect.Height, img3.Width, img3.Height));
                         }
                     }
                 }
@@ -141,6 +144,41 @@ namespace ZSS
                 Console.WriteLine(ex.ToString());
             }
             return img;
+        }
+
+        public static Bitmap Transparent(Bitmap bmp)
+        {
+            bmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+            Bitmap b = bmp.Clone(new Rectangle(0, 0, bmp.Width, bmp.Height / 2), PixelFormat.Format32bppArgb);
+            BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+            int stride = bmData.Stride;
+            System.IntPtr Scan0 = bmData.Scan0;
+            byte alpha;
+
+            unsafe
+            {
+                byte* p = (byte*)(void*)Scan0;
+                int nOffset = stride - b.Width * 4;
+
+                for (int y = 0; y < b.Height; ++y)
+                {
+                    for (int x = 0; x < b.Width; ++x)
+                    {
+                        alpha = (byte)(255 - 255 * (y + 1) / b.Height);
+                        if (p[3] > alpha)
+                        {
+                            p[3] = alpha;
+                        }
+                        p += 4;
+                    }
+                    p += nOffset;
+                }
+            }
+
+            b.UnlockBits(bmData);
+
+            return b;
         }
     }
 }
