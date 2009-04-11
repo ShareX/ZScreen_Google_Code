0d8220ec5a4b9fe9d9c29151fa44bfb16a9dcc08
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-04-11 03:58:08 +0000

    2.6.19.1 Fixed possible crash when Images folder was moved and ZScreen tries to load History.xml

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.6.4.348")]
-[assembly: AssemblyFileVersion("2.6.19.0")]
+[assembly: AssemblyVersion("2.6.4.353")]
+[assembly: AssemblyFileVersion("2.6.19.1")]
