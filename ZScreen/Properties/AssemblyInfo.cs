343207b3f0222f034457e42db4420410f91e05a4
McoreD <mcored@gmail.com>
2009-04-14 14:17:48 +0000

    2.8.00.0 Magnifying Glass feature for Crop Shot

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.6.4.385")]
-[assembly: AssemblyFileVersion("2.7.1.0")]
+[assembly: AssemblyVersion("2.6.4.388")]
+[assembly: AssemblyFileVersion("2.8.0.0")]
