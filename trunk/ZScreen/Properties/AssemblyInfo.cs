e30749a3577943c996bf9986ac5b94b8e1912c79
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-04-24 05:49:23 +0000

    AutoChangeUploadDestination = true;

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.6.4.416")]
+[assembly: AssemblyVersion("2.6.4.423")]
 [assembly: AssemblyFileVersion("2.10.2.0")]
