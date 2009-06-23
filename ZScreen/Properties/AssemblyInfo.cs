dc442f1849234cc7679ddd88c3fdc52da539aa14
McoreD <mcored@gmail.com>
2009-06-23 11:08:13 +0000

    Updated VersionHistory: 2.11.00.0 Clipboard Upload to paste2.org and pastebin.com

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.6.4.460")]
-[assembly: AssemblyFileVersion("2.10.10.1")]
+[assembly: AssemblyVersion("2.6.4.472")]
+[assembly: AssemblyFileVersion("2.11.0.0")]
