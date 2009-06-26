5315abaec4b16c2cd5eca9b4e8bc0530afc168e7
McoreD <mcored@gmail.com>
2009-06-26 16:47:54 +0000

    xml settings are fucked up again

--- ZScreen/Forms/TextViewer.Designer.cs
+++ ZScreen/Forms/TextViewer.Designer.cs
@@ -35,12 +35,12 @@
             // 
             this.txtText.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtText.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.txtText.Location = new System.Drawing.Point(0, 0);
+            this.txtText.Location = new System.Drawing.Point(4, 4);
             this.txtText.Multiline = true;
             this.txtText.Name = "txtText";
             this.txtText.ReadOnly = true;
             this.txtText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtText.Size = new System.Drawing.Size(632, 446);
+            this.txtText.Size = new System.Drawing.Size(624, 438);
             this.txtText.TabIndex = 0;
             this.txtText.TabStop = false;
             this.txtText.WordWrap = false;
@@ -53,6 +53,7 @@
             this.Controls.Add(this.txtText);
             this.MinimumSize = new System.Drawing.Size(640, 480);
             this.Name = "frmTextViewer";
+            this.Padding = new System.Windows.Forms.Padding(4);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmTextViewer";
             this.ResumeLayout(false);
