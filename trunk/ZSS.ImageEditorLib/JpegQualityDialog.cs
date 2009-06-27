31bdaf45f7a048f751c7ff610335ee39efe7b43b
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-06-27 08:51:37 +0000

    Image Editor based on Greenshot Image Editor 0.7.009 was moved to a separate Dynamic Link Library

---
+++ trunk/ZSS.ImageEditorLib/JpegQualityDialog.cs
@@ -0,0 +1,55 @@
+/*
+ * Created by SharpDevelop.
+ * User: jens
+ * Date: 19.07.2007
+ * Time: 21:37
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Greenshot.Configuration;
+
+namespace Greenshot
+{
+	/// <summary>
+	/// Description of JpegQualityDialog.
+	/// </summary>
+	public partial class JpegQualityDialog : Form
+	{
+		AppConfig conf;
+		public int Quality = 0;
+		public JpegQualityDialog()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			conf = AppConfig.GetInstance();
+
+			this.trackBarJpegQuality.Value = conf.Output_File_JpegQuality;
+			this.textBoxJpegQuality.Text = conf.Output_File_JpegQuality.ToString();
+
+		}
+		
+		
+		void Button_okClick(object sender, System.EventArgs e)
+		{
+			Quality = this.trackBarJpegQuality.Value;
+			if(this.checkbox_dontaskagain.Checked) {
+				conf.Output_File_JpegQuality = Quality;
+				conf.Output_File_PromptJpegQuality = false;
+				conf.Store();
+			}
+			
+		}
+		
+		void TrackBarJpegQualityScroll(object sender, System.EventArgs e)
+		{
+			textBoxJpegQuality.Text = trackBarJpegQuality.Value.ToString();
+		}
+	}
+}
