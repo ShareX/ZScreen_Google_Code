5419f1578966b44f65963756649996957d90b2ff
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-06-28 15:50:21 +0000

    this.Cursor = new Cursor(Helpers.GetImageResource("ZSS.ColorsLib.Cursor.cur"));

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.6.4.575")]
+[assembly: AssemblyVersion("2.6.4.580")]
 [assembly: AssemblyFileVersion("2.15.0.0")]
