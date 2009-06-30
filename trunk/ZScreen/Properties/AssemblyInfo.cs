e8d002d4abb40e4845dc97805abdd7f381202f46
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-06-30 08:00:56 +0000

    (unsuccessful) BitlyUploader code

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.6.4.603")]
+[assembly: AssemblyVersion("2.6.4.616")]
 [assembly: AssemblyFileVersion("2.15.1.0")]
