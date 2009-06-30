42d50ea77aa9daa2beec9e0fe78ec5cea99c00a3
rgrthat <rgrthat@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-06-30 07:20:39 +0000

    Updated controls on Main tab to look more uniform

--- trunk/ZScreen/UserControls/NumericUpDownTimer.Designer.cs
+++ trunk/ZScreen/UserControls/NumericUpDownTimer.Designer.cs
@@ -28,31 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblDelay = new System.Windows.Forms.Label();
             this.nudDelay = new System.Windows.Forms.NumericUpDown();
             this.cbDelay = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.nudDelay)).BeginInit();
             this.SuspendLayout();
             // 
-            // lblDelay
-            // 
-            this.lblDelay.AutoSize = true;
-            this.lblDelay.Location = new System.Drawing.Point(4, 6);
-            this.lblDelay.Name = "lblDelay";
-            this.lblDelay.Size = new System.Drawing.Size(37, 13);
-            this.lblDelay.TabIndex = 0;
-            this.lblDelay.Text = "Delay:";
-            // 
             // nudDelay
             // 
-            this.nudDelay.Location = new System.Drawing.Point(44, 3);
+            this.nudDelay.Location = new System.Drawing.Point(2, 3);
             this.nudDelay.Maximum = new decimal(new int[] {
             1000000000,
             0,
             0,
             0});
             this.nudDelay.Name = "nudDelay";
-            this.nudDelay.Size = new System.Drawing.Size(120, 20);
+            this.nudDelay.Size = new System.Drawing.Size(68, 20);
             this.nudDelay.TabIndex = 1;
             this.nudDelay.ValueChanged += new System.EventHandler(this.nudDelay_ValueChanged);
             // 
@@ -61,9 +51,9 @@
             this.cbDelay.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbDelay.FormattingEnabled = true;
             this.cbDelay.ItemHeight = 13;
-            this.cbDelay.Location = new System.Drawing.Point(172, 3);
+            this.cbDelay.Location = new System.Drawing.Point(74, 3);
             this.cbDelay.Name = "cbDelay";
-            this.cbDelay.Size = new System.Drawing.Size(121, 21);
+            this.cbDelay.Size = new System.Drawing.Size(159, 21);
             this.cbDelay.TabIndex = 2;
             this.cbDelay.SelectedIndexChanged += new System.EventHandler(this.cbDelay_SelectedIndexChanged);
             // 
@@ -73,18 +63,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.cbDelay);
             this.Controls.Add(this.nudDelay);
-            this.Controls.Add(this.lblDelay);
             this.Name = "NumericUpDownTimer";
-            this.Size = new System.Drawing.Size(297, 27);
+            this.Size = new System.Drawing.Size(233, 27);
             ((System.ComponentModel.ISupportInitialize)(this.nudDelay)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label lblDelay;
         private System.Windows.Forms.NumericUpDown nudDelay;
         private System.Windows.Forms.ComboBox cbDelay;
     }
