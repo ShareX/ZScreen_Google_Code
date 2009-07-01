aaddd80c558575eeaec1ae96989ec1c4e2735826
McoreD <mcored@gmail.com>
2009-07-01 07:27:57 +0000

    ready to release 2.15.01.1 Hotkeys tab was not displayed propery for DPI set at 120% [Issue 82]

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.6.4.636")]
+[assembly: AssemblyVersion("2.6.4.638")]
 [assembly: AssemblyFileVersion("2.15.1.1")]
