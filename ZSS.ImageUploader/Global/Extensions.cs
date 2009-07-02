f9e107aee2233340382fc4e54aa228b06be72ef8
Jaex <Jaex@users.noreply.github.com>
2009-07-02 05:08:30 +0000

    TwitPic ParseResult fix

--- ZSS.ImageUploader/Global/Extensions.cs
+++ ZSS.ImageUploader/Global/Extensions.cs
@@ -20,5 +20,29 @@ namespace ZSS.ImageUploaderLib
                 return "";
             }
         }
+
+        public static string AttributeValue(this XElement xe, string name)
+        {
+            XAttribute xeItem = xe.Attribute(name);
+            if (xeItem != null)
+            {
+                return xeItem.Value;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public static string AttributeFirstValue(this XElement xe, params string[] names)
+        {
+            string value;
+            foreach (string name in names)
+            {
+                value = xe.AttributeValue(name);
+                if (!string.IsNullOrEmpty(value)) return value;
+            }
+            return "";
+        }
     }
 }
