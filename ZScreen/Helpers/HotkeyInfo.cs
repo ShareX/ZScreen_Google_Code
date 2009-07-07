4ce01a9709c01d39c1e595a2543eeba4f2259073
McoreD <mcored@gmail.com>
2009-07-07 14:41:00 +0000

    version history fix; serializable HotkeyInfo class for future use

---
+++ ZScreen/Helpers/HotkeyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ZSS.Helpers
+{
+    [Serializable]
+    public class HotkeyInfo
+    {
+        public string Name { get; set; }
+        public Keys Key { get; set; }
+
+        public HotkeyInfo() { }
+
+        public HotkeyInfo(string myName, Keys myHotkey)
+        {
+            this.Name = myName;
+            this.Key = myHotkey;
+        }
+    }
+}
