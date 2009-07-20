5dcf42fc1cef14e4c5769c3c3c955dd16964bb54
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-07-20 13:46:56 +0000

    2.21.2.0 More efficient image uploading mechanism {Jaex}

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -53,4 +53,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("2.20.0.804")]
-[assembly: AssemblyFileVersion("2.21.0.2")]
+[assembly: AssemblyFileVersion("2.21.2.0")]
