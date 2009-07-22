206527e49f269b4c6a871894f8112fa830b522c4
rgrthat <rgrthat@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-07-22 02:14:59 +0000

    Added error messages inside of a switch statement for TinyPic.

--- trunk/ZSS.ImageUploader/ImageUploaders/TinyPicUploader.cs
+++ trunk/ZSS.ImageUploader/ImageUploaders/TinyPicUploader.cs
@@ -30,6 +30,7 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Web;
 using ZSS.ImageUploadersLib.Helpers;
+using System.Collections;
 
 namespace ZSS.ImageUploadersLib
 {
@@ -95,28 +96,32 @@ namespace ZSS.ImageUploadersLib
 
                 string response = GetResponse(URLAPI, args);
 
+
                 if (string.IsNullOrEmpty(response)) throw new Exception("Unable to get upload key.");
 
-                string upk = GetXMLValue(response, "uploadkey");
-
-                if (string.IsNullOrEmpty(upk))
+                if (GetXMLValue(response, "status") == "OK")
                 {
-                    throw new Exception("Upload Key is Empty.");
-                }
 
-                if (string.IsNullOrEmpty(Shuk))
-                {
-                    action = "upload"; //anonymous upload
-                }
-                else
-                {
-                    action = "userupload"; //user upload
-                }
+                    string upk = GetXMLValue(response, "uploadkey");
 
-                ServicePointManager.Expect100Continue = false;
+                    if (string.IsNullOrEmpty(upk))
+                    {
+                        throw new Exception("Upload Key is Empty.");
+                    }
 
-                Dictionary<string, string> arguments = new Dictionary<string, string>() 
-                {                 
+                    if (string.IsNullOrEmpty(Shuk))
+                    {
+                        action = "upload"; //anonymous upload
+                    }
+                    else
+                    {
+                        action = "userupload"; //user upload
+                    }
+
+                    ServicePointManager.Expect100Continue = false;
+
+                    Dictionary<string, string> arguments = new Dictionary<string, string>() 
+                    {                 
                     { "action", action },
                     { "tpid", tpid },
                     //{ "tpk", tpk },
@@ -124,21 +129,29 @@ namespace ZSS.ImageUploadersLib
                     { "upk", upk },
                     { "tags", "" },
                     { "type", "image" }
-                };
+                    };
 
-                if (!string.IsNullOrEmpty(Shuk))
-                {
-                    arguments.Add("shuk", Shuk);
+                    if (!string.IsNullOrEmpty(Shuk))
+                    {
+                        arguments.Add("shuk", Shuk);
+                    }
+
+                    ifm.Source = PostImage(image, URLAPI, "uploadfile", arguments);
+                    //ifm.Source = new TCPClient(this).UploadImage(image, URLAPI, "uploadfile", mFileName, arguments);
+
+                    string fullimage = GetXMLValue(ifm.Source, "fullsize");
+                    string thumbnail = GetXMLValue(ifm.Source, "thumbnail");
+
+                    if (!string.IsNullOrEmpty(fullimage)) ifm.ImageFileList.Add(new ImageFile(fullimage, ImageFile.ImageType.FULLIMAGE));
+                    if (!string.IsNullOrEmpty(thumbnail)) ifm.ImageFileList.Add(new ImageFile(thumbnail, ImageFile.ImageType.THUMBNAIL));
                 }
+                else if(GetXMLValue(response, "status") == "FAIL")
+                {
+                    int errorCode = -1;
+                    Int32.TryParse(GetXMLValue(response, "errorcode"), out errorCode);
 
-                ifm.Source = PostImage(image, URLAPI, "uploadfile", arguments);
-                //ifm.Source = new TCPClient(this).UploadImage(image, URLAPI, "uploadfile", mFileName, arguments);
-
-                string fullimage = GetXMLValue(ifm.Source, "fullsize");
-                string thumbnail = GetXMLValue(ifm.Source, "thumbnail");
-
-                if (!string.IsNullOrEmpty(fullimage)) ifm.ImageFileList.Add(new ImageFile(fullimage, ImageFile.ImageType.FULLIMAGE));
-                if (!string.IsNullOrEmpty(thumbnail)) ifm.ImageFileList.Add(new ImageFile(thumbnail, ImageFile.ImageType.THUMBNAIL));
+                    this.Errors.Add(GetErrorMessage(errorCode));
+                }
             }
             catch (Exception ex)
             {
@@ -227,6 +240,61 @@ namespace ZSS.ImageUploadersLib
             }
 
             return "";
+        }
+
+        public string GetErrorMessage(int errorCode)
+        {
+            switch (errorCode)
+            {
+                case 1: 
+                    return "Bad Authorization: Missing or invalid authentication parameter (tpid, tpk, sig, upk).";
+                case 2:
+                    return "Bad Action: Invalid or no action passed.";
+                case 3:
+                    return "Bad Type: Invalid or no type passed.";
+                case 4:
+                    return "Bad File: No upload file included in POST when required.";
+                case 5: 
+                    return "Bad Term: No search term included with search request.";
+                case 6: 
+                    return "Bad Media Key: Media key not included in a request that required a media key.";
+                case 7:
+                    return "Bad Upload Key: Invalid or no upload key in a request that required an upload key.";
+                case 8: 
+                    return "Bad User Credentials: Invalid or no user credentials (username or pass) in a request that required username/pass to be valid.";
+                case 9:
+                    return "Insufficient Permission: Action called that the specific tpid does not have permission to perform.";
+                case 10:
+                    return "Image Processing Error: Uploaded image fails to process correctly.";
+                case 11: 
+                    return "Video Encoding Error: Unable to encode video.";
+                case 12: 
+                    return "Bad Album: Invalid or no albumid is passed when required.";
+                case 20: 
+                    return "Bad Email: Invalid or no email passed in register action.";
+                case 21: 
+                    return "Duplicate Email: Email passed in register action is already in use by another registered user.";
+                case 22:
+                    return "Bad Password: Password passed in register action is less than six characters.";
+                case 23:
+                    return "Bad Birth Date: Birth date fields passed in register action combine to make an invalid date (e.g., February 30, 1990).";
+                case 24:
+                    return "User too Young: Birth date fields passed in register action place a user below the minimum age of 14 years.";
+                case 25:
+                    return "Terms Agreement: Terms parameter for register action is either blank, non-existent, or does not say \"yes\"";
+                case 26: 
+                    return "Terms of Service Violation: originatingip parameter contains an IP address on the TinyPic banned list, or is in a nation that is banned from registering.";
+                case 27:
+                    return "Bad IP: originationip parameter contains an invalid IP address.";
+                case 28:
+                    return "Bad Album Name: Album name that is passed in a user album action is too short (less than two characters).";
+                case 99:
+                    return "Internal Error: An internal error occurred in processing request.";
+                case 100:
+                    return "No File: mediakey is passed in an action, but the media is not found/no longer available.";
+                default:
+                    return "No error message was provided.";
+            }
         }
     }
 }
