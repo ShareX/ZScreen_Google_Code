ba7829941ef73db761a9de982f235623ccd1766f
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-07-25 11:44:33 +0000

    exposed all indexer configuration properties; removed empty line when indexing to text files; footer is now optional

--- trunk/ZSS.IndexersLib/Helpers/IndexerConfig.cs
+++ trunk/ZSS.IndexersLib/Helpers/IndexerConfig.cs
@@ -2,46 +2,46 @@
 using System.Collections.Generic;
 using System.IO;
 using System;
+using System.ComponentModel;
+using System.Xml.Serialization;
 
 namespace ZSS.IndexersLib
 {
     [Serializable()]
     public class IndexerConfig
-    {        
+    {
         //Default to Tree.NET
-        public System.Threading.ThreadPriority ProcessPriority;
-
-        // Main Form 
-        private List<string> m_FolderList = new List<string>();
-
-        private string m_IndexFileExtension = ".html";
-        //Because default is Tree.NET
-        private string m_IndexFileName = "index";
-
+        [Category("Engines / General"), Description("Adjust CPU indexing priority level")]
+        public System.Threading.ThreadPriority ProcessPriority { get; set; }
+     
         // Config > Engine > Tree 
-        private bool m_AddFilesTree = true;
-        private bool m_Ascii = true;
-
-        // Config > Output 
-        private bool m_IndexFileInSameDir = true;
-        public bool CreateIndividualFilesInOutputDir = true;
+        [Category("Engines / Tree Walk Utility"), DefaultValue(true), Description("Display the names of the files in each folder.")]
+        public bool TreeShowFiles { get; set; }
+        [Category("Engines / Tree Walk Utility"), DefaultValue(true), Description("Use ASCII instead of extended characters")]
+        public bool TreeUseAscii { get; set; }
+        [Category("Engines / Tree Walk Utility"), DefaultValue(false), Description("Remove tree branches")]
+        public bool RemoveTreeBranches { get; set; }
 
         // Engine > TreeNet 
-        public bool ShowFilesTreeNet = true;
-        public bool ShowFileSize = true;
-        public bool ShowDirSize = true;
+        [Category("Engines / Tree.NET"), DefaultValue(true), Description("Display files")]
+        public bool ShowFilesTreeNet { get; set; }
+        [Category("Engines / Tree.NET"), DefaultValue(true), Description("Display file sizes")]
+        public bool ShowFileSize { get; set; }
+        [Category("Engines / Tree.NET"), DefaultValue(true), Description("Display folder sizes")]
+        public bool ShowDirSize { get; set; }
+        [Category("Engines / Tree.NET"), DefaultValue(false), Description("Show full file path in the index")]
         public bool ShowFilePath { get; set; }
+        [Category("Engines / Tree.NET"), DefaultValue(false), Description("Show full folder path in the index")]
         public bool ShowFolderPath { get; set; }
+        [Category("Engines / Tree.NET"), DefaultValue(false), Description("Hide Extension in files")]
         public bool HideExtension { get; set; }
+        [Category("Engines / Tree.NET"), DefaultValue(false), Description("Show virtual folders")]
         public bool ShowVirtualFolders { get; set; }
+        [Category("Engines / Tree.NET"), DefaultValue(false), Description("Show folder paths in the browser's status bar")]
         public bool ShowFolderPathOnStatusBar { get; set; }
-
-        public string FolderHeadingStyle = "*~";
-
-        // Engine > TreeNet > XHTML Output
-
-        private eListType mListType = eListType.Bullets;
-
+        [Category("Engines / Tree.NET"), DefaultValue("~*"), Description("Folder heading style")]
+        public string FolderHeadingStyle { get; set; }
+       
         // Zip Operations
         public bool ZipAfterIndexed = false;
         public bool ZipAndDeleteFile = false;
@@ -50,58 +50,49 @@ namespace ZSS.IndexersLib
         public bool ZipFilesInOutputDir = false;
 
         // Filter
-
         public bool EnabledFiltering = true;
-        public bool HideProtectedOperatingSystemFilesFolders = false;
-
-        public bool IgnoreHiddenFiles = false;
-        public bool IgnoreSystemFiles = false;
+        [Category("General / Filter"), DefaultValue(false), Description("Ignore protected Operating System files and folders when indexing")]
+        public bool HideProtectedOperatingSystemFilesFolders { get; set; }
+        [Category("General / Filter"), DefaultValue(false), Description("Ignore hidden files when indexing")]
+        public bool IgnoreHiddenFiles { get; set; }
+        [Category("General / Filter"), DefaultValue(false), Description("Ignore system files when indexing")]
+        public bool IgnoreSystemFiles { get; set; }
         public bool IgnoreFollowingFiles = true;
-        const string mDefaultCssFileName = "Default.css";
+        public const string mDefaultCssFileName = "Default.css";
         public string IgnoreFilesList = ".DS_Store|*.db|index.html|*.ini";
-        public bool IgnoreHiddenFolders = false;
-        public bool IgnoreSystemFolders = false;
-        public bool IgnoreEmptyFolders = false;
+        [Category("General / Filter"), DefaultValue(false), Description("Ignore system folders when indexing")]
+        public bool IgnoreHiddenFolders { get; set; }
+        [Category("General / Filter"), DefaultValue(false), Description("Ignore system folders when indexing")]
+        public bool IgnoreSystemFolders { get; set; }
+        [Category("General / Filter"), DefaultValue(false), Description("Ignore empty folders when indexing")]
+        public bool IgnoreEmptyFolders { get; set; }
         //war59312
 
         // Misc
-        public string MergedHtmlTitle = "Site Index";
-        public string ServerInfo = "ftp://127.0.0.1:21";
+        [Category("Engines / Tree.NET / XHTML"), Description("Merged XHTML Title")]
+        public string MergedHtmlTitle { get; set; }
+        [Category("Engines / Tree.NET / XHTML"), Description("Service information e.g. ftp://domain:port")]
+        public string ServerInfo { get; set; }
 
+        [Category("General / Display"), DefaultValue(false), Description("Display files in reverse")]
         public bool RevereFileOrder = false;
+        [Category("General / Display"), DefaultValue(false), Description("Display file count")]
         public bool ShowFileCount = false;
-
-        private List<string> mVirtualFolderList = new List<string>();
-
-        public bool mSortBySize = false;
-        public int mSortBySizeMode = 1;
-        //  0 = Ascending, 1 = Descending
-
+        [Category("General / Tree.NET / Display"), DefaultValue(false), Description("Display file count")]
+        public bool SortBySize { get; set; }
+        [Category("General / Tree.NET / Display"), DefaultValue( FileSortMode.Ascending), Description("Sort files by Size")]
+        public FileSortMode SortBySizeMode { get; set; }
+        
+        [Category("General / Audio"), DefaultValue(false), Description("Enable Quick Audio scan to index faster. Bitrate information will not be precise.")]
         public bool AudioQuickScan { get; set; }
-        public bool AudioInfo = true;
-
-        private int mFolderExpandLevel = 2;
-        public int FolderExpandLevel
-        {
-            get { return mFolderExpandLevel; }
-            set { mFolderExpandLevel = value; }
-        }
-
-        private bool mCollapseFolders = false;
-        public bool CollapseFolders
-        {
-            get { return mCollapseFolders; }
-            set { mCollapseFolders = value; }
-        }
-
-        private string mLogoPath;
-        public string LogoPath
-        {
-            get { return mLogoPath; }
-            set { mLogoPath = value; }
-        }
-
-
+        [Category("General / Audio"), DefaultValue(false), Description("Enable Audio file scan.")]
+        public bool AudioInfo { get; set; }
+        [Category("Engines / Tree.NET"), DefaultValue(2), Description("Folder expand level")]
+        public int FolderExpandLevel { get; set; }
+        [Category("Engines / Tree.NET"), DefaultValue(false), Description("Collapse folders")]
+        public bool CollapseFolders { get; set; }
+        [Category("Engines / Tree.NET"), Description("Path for an image logo")]
+        public string LogoPath { get; set; }
 
         // 2.7.1.4 Default CSS file is read from Application startup path 
         // default.css when current file does not exit
@@ -109,19 +100,37 @@ namespace ZSS.IndexersLib
 
         public IndexerConfig()
         {
+            IndexingEngineType = IndexingEngine.TreeNetLib;
+
+            TreeShowFiles = true;
+            TreeUseAscii = true; 
+
+            AudioInfo = true;
+            ShowFilesTreeNet = true;
+            ShowFileSize = true;
+            ShowDirSize = true;
+
             AudioQuickScan = true;
+            IndexFileExt = ".html";
+            FolderExpandLevel = 2;
+            FolderHeadingStyle = "*~";
+            IndexListType = XHTMLFileListMode.Bullets;
+            ServerInfo = "ftp://127.0.0.1:21";
+            MergedHtmlTitle = "Site Index";
+            VirtualFolderList = new List<string>();
+            FolderList = new List<string>();
         }
 
+        [Category("Engines / Tree.NET / XHTML"), Description("Cascading Style Sheet file name")]
         public string CssFileName
         {
             get { return mDefaultCssFileName; }
         }
 
+        [Category("Engines / Tree.NET / XHTML"), Description("Cascading Style Sheet file path")]
         public string CssFilePath
         {
-
             get { return this.mCssFilePath; }
-
             set
             {
                 if (File.Exists(value))
@@ -135,44 +144,60 @@ namespace ZSS.IndexersLib
             }
         }
 
+        [Category("Engines / Tree.NET / XHTML"), Description("Virtual folders list")]
+        public List<string> VirtualFolderList = null;
 
+        // Engine > TreeNet > XHTML Output
+        [Category("Engines / Tree.NET / XHTML"), DefaultValue(XHTMLFileListMode.Bullets), Description("Display the files as a numbered list or a bulletted list")]
+        public XHTMLFileListMode IndexListType { get; set; }
 
-        public List<string> VirtualFolderList
-        {
-            get { return mVirtualFolderList; }
-            set { mVirtualFolderList = value; }
-        }
+        public List<string> FolderList = null;
 
+        [Category("Engines / General"), DefaultValue(false), Description("Show footer message showing project page")]
+        public bool AddFooter { get; set; }
+        [Category("Engines / General"), Description("Index file name without extension")]
+        public string IndexFileNameWitoutExt { get; set; }
+        [Category("Engines / General"), Description("Index file extension")]
+        public string IndexFileExt { get; set; }
 
-        public enum eListType
-        {
-            Bullets,
-            Numbered
-        }
+        [Category("Engines / General"), DefaultValue(false), Description("Merge index file")]
+        public bool MergeFiles { get; set; }
+        [Category("Engines / General"), Description("Output directory for merge index file")]
+        public string OutputDir { get; set; }
+        [Category("Engines / General"), Description("Indexing Engine")]
+        public IndexingEngine IndexingEngineType { get; set; }
 
-        public eListType ListType
-        {
-            get { return mListType; }
-            set { mListType = value; }
-        }
+        #region "Methods"
 
         public string GetIndexFileName()
         {
-            return this.m_IndexFileName + this.m_IndexFileExtension;
+            return this.IndexFileNameWitoutExt + IndexFileExt;
         }
 
         public string[] GetIndexFilePaths()
         {
-
             string[] paths = new string[FolderList.Count];
 
             for (int i = 0; i <= FolderList.Count - 1; i++)
             {
                 paths[i] = FolderList[i] + Path.DirectorySeparatorChar + this.GetIndexFileName();
             }
-
-
             return paths;
+        }
+
+        public void SetIndexPath(string fpath)
+        {
+            IndexFileNameWitoutExt = Path.GetFileNameWithoutExtension(fpath);
+            IndexFileExt = Path.GetExtension(fpath);
+        }
+        public void SetSingleIndexPath(string fpath)
+        {
+
+            OutputDir = Path.GetDirectoryName(fpath);
+            IndexFileNameWitoutExt = Path.GetFileNameWithoutExtension(fpath);
+            IndexFileExt = Path.GetExtension(fpath);
+
+            MergeFiles = true;
         }
 
         public string GetIndexFilePath()
@@ -180,62 +205,7 @@ namespace ZSS.IndexersLib
             return this.OutputDir + Path.DirectorySeparatorChar + this.GetIndexFileName();
         }
 
-        public List<string> FolderList
-        {
-            get { return m_FolderList; }
-            set { this.m_FolderList = value; }
-        }
-
-
-        public string IndexFileName
-        {
-            get { return this.m_IndexFileName; }
-            set { this.m_IndexFileName = value; }
-        }
-
-        public string IndexFileExtension
-        {
-
-            get { return this.m_IndexFileExtension; }
-            set { m_IndexFileExtension = value; }
-        }
-
-        public bool isIndexFileInSameDir
-        {
-            get { return m_IndexFileInSameDir; }
-            set { this.m_IndexFileInSameDir = value; }
-        }
-
-        private bool isIndexFileInOneDir { get; set; }
-
-        public bool RemoveTreeBranches { get; set; }
-
-        public bool isMergeFiles { get; set; }
-        public bool isAscii
-        {
-            get { return this.m_Ascii; }
-            set { m_Ascii = value; }
-        }
-
-        public bool isAddFiles
-        {
-            get { return this.m_AddFilesTree; }
-            set { m_AddFilesTree = value; }
-        }
-
-        public string OutputDir { get; set; }
-
-        public IndexingEngine IndexingEngineID { get; set; }
-
-        public void SetSingleIndexPath(string fpath)
-        {
-
-            OutputDir = Path.GetDirectoryName(fpath);
-            IndexFileName = Path.GetFileNameWithoutExtension(fpath);
-            IndexFileExtension = Path.GetExtension(fpath);
-
-            isMergeFiles = true;
-        }
+        #endregion
     }
 }
 
