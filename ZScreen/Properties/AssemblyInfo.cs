2e1f781b7667d7ef2326a1a04f2856d7ba313e03
McoreD <mcored@gmail.com>
2009-08-07 20:17:46 +0000

    conflict resolution for 2.28.1.0 Added 3D turn effect to ZScreen Logo {Jaex}

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.25.0.962")]
-[assembly: AssemblyFileVersion("2.29.0.0")]
+[assembly: AssemblyVersion("2.25.0.963")]
+[assembly: AssemblyFileVersion("2.29.1.0")]
