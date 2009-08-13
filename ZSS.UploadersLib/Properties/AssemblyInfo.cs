e95f6ac9ef13f01669df6ca89a985bf21c4e0612
McoreD <mcored@gmail.com>
2009-08-13 23:14:55 +0000

    removed unused csprojs; UploadFile respects MakeTinyURL setting

--- ZSS.UploadersLib/Properties/AssemblyInfo.cs
+++ ZSS.UploadersLib/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ZSS.UploadersLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Dynamic Link Library for image/text/file hosting services")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ZSS.UploadersLib")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
