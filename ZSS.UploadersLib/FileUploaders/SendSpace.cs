b7be94dd81607ada5b4aa9809e04272a83f9c31b
Jaex <Jaex@users.noreply.github.com>
2009-08-14 11:04:26 +0000

    More sendspace code for make seperate test project

--- ZSS.UploadersLib/FileUploaders/SendSpace.cs
+++ ZSS.UploadersLib/FileUploaders/SendSpace.cs
@@ -28,7 +28,7 @@ using System.Text;
 
 namespace UploadersLib.FileUploaders
 {
-    public class SendSpace : Uploader
+    public class SendSpace : FileUploader
     {
         private const string SENDSPACE_API_KEY = "LV6OS1R0Q3";
         private const string SENDSPACE_API_URL = "http://api.sendspace.com/rest/";
@@ -39,6 +39,11 @@ namespace UploadersLib.FileUploaders
         public SendSpace()
         {
 
+        }
+
+        public override string Name
+        {
+            get { return "SendSpace"; }
         }
 
         #region Authentication
@@ -155,6 +160,49 @@ namespace UploadersLib.FileUploaders
             string response = GetResponse(SENDSPACE_API_URL, args);
 
             return "";
+        }
+
+        /// <summary>
+        /// http://www.sendspace.com/dev_method.html?method=upload.getInfo
+        /// </summary>
+        /// <param name="max_file_size">max_file_size value received in UploadGetInfo response</param>
+        /// <param name="upload_identifier">upload_identifier value received in UploadGetInfo response</param>
+        /// <param name="extra_info">extra_info value received in UploadGetInfo response</param>
+        /// <param name="description"></param>
+        /// <param name="password"></param>
+        /// <param name="folder_id"></param>
+        /// <param name="recipient_email">an email (or emails separated with ,) of recipient/s to receive information about the upload</param>
+        /// <param name="notify_uploader">0/1 - should the uploader be notified?</param>
+        /// <param name="redirect_url">page to redirect after upload will be attached upload_status=ok/fail&file_id=XXXX</param>
+        /// <returns></returns>
+        public Dictionary<string, string> PrepareArguments(string max_file_size, string upload_identifier, string extra_info,
+            string description, string password, string folder_id, string recipient_email, string notify_uploader, string redirect_url)
+        {
+            Dictionary<string, string> args = new Dictionary<string, string>();
+            args.Add("MAX_FILE_SIZE", max_file_size);
+            args.Add("UPLOAD_IDENTIFIER", upload_identifier);
+            args.Add("extra_info", extra_info);
+
+            // Optional fields
+
+            if (!string.IsNullOrEmpty(description)) args.Add("description", description);
+            if (!string.IsNullOrEmpty(password)) args.Add("password", password);
+            if (!string.IsNullOrEmpty(folder_id)) args.Add("folder_id", folder_id);
+            if (!string.IsNullOrEmpty(recipient_email)) args.Add("recipient_email", recipient_email);
+            if (!string.IsNullOrEmpty(notify_uploader)) args.Add("notify_uploader", notify_uploader);
+            if (!string.IsNullOrEmpty(redirect_url)) args.Add("redirect_url", redirect_url);
+
+            return args;
+        }
+
+        public Dictionary<string, string> PrepareArguments(string max_file_size, string upload_identifier, string extra_info)
+        {
+            return PrepareArguments(max_file_size, upload_identifier, extra_info, null, null, null, null, null, null);
+        }
+
+        public override string Upload(byte[] data, string fileName)
+        {
+
         }
 
         #endregion
