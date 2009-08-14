9211bb5446151073b2de9d829e6b0456677b63ca
flexy123 <flexy123@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-08-14 18:21:07 +0000

    Added anonymous upload for SendSpace

--- branches/UploaderTester/UploaderTester/Program.cs
+++ branches/UploaderTester/UploaderTester/Program.cs
@@ -10,16 +10,24 @@ namespace UploaderTester
     {
         static void Main(string[] args)
         {
+            string url;
             SendSpace sendSpace = new SendSpace();
-            sendSpace.AuthRegister("Jaex", "Berk", "blabla@gmail.com", "pass");
-            string token = sendSpace.AuthCreateToken();
-            SendSpace.LoginInfo loginInfo = sendSpace.AuthLogin(token, "Jaex", "pass");
-            string sessionKey = loginInfo.SessionKey;
-            if (sendSpace.AuthCheckSession(sessionKey))
+
+            try
             {
-                sendSpace.CurrentUploadInfo = sendSpace.UploadGetInfo(sessionKey, "0");
-                string url = sendSpace.Upload(@"C:\Users\PC\Desktop\test.rar");
+                string token = sendSpace.AuthCreateToken();
+                SendSpace.LoginInfo loginInfo = sendSpace.AuthLogin(token, "Jaex", "pass");
+                sendSpace.CurrentUploadInfo = sendSpace.UploadGetInfo(loginInfo.SessionKey);
+                url = sendSpace.Upload(@"C:\Users\PC\Desktop\test.rar");
                 Console.WriteLine("Success: {0}", url);
+
+                sendSpace.CurrentUploadInfo = sendSpace.AnonymousUploadGetInfo();
+                url = sendSpace.Upload(@"C:\Users\PC\Desktop\test.rar");
+                Console.WriteLine("Success: {0}", url);
+            }
+            catch
+            {
+                Console.WriteLine(sendSpace.ToErrorString());
             }
 
             Console.ReadLine();
