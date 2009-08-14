dd28b542cbcd533a123e8b3934a422d1235bb719
flexy123 <flexy123@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-08-14 17:57:40 +0000

    SendSpace upload working

--- branches/UploaderTester/UploaderTester/Program.cs
+++ branches/UploaderTester/UploaderTester/Program.cs
@@ -11,7 +11,16 @@ namespace UploaderTester
         static void Main(string[] args)
         {
             SendSpace sendSpace = new SendSpace();
-
+            sendSpace.AuthRegister("Jaex", "Berk", "blabla@gmail.com", "pass");
+            string token = sendSpace.AuthCreateToken();
+            SendSpace.LoginInfo loginInfo = sendSpace.AuthLogin(token, "Jaex", "pass");
+            string sessionKey = loginInfo.SessionKey;
+            if (sendSpace.AuthCheckSession(sessionKey))
+            {
+                sendSpace.CurrentUploadInfo = sendSpace.UploadGetInfo(sessionKey, "0");
+                string url = sendSpace.Upload(@"C:\Users\PC\Desktop\test.rar");
+                Console.WriteLine("Success: {0}", url);
+            }
 
             Console.ReadLine();
         }
