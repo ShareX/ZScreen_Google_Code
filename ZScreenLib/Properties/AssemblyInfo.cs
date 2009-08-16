f52c3b5bfac05844944af65c7e55a5f84a3db827
McoreD <mcored@gmail.com>
2009-08-16 10:25:47 +0000

    moved SVNToAssemblyInfo to ZScreenLib

--- ZScreenLib/Properties/AssemblyInfo.cs
+++ ZScreenLib/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ZScreenLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Advanced image/text uploading utility")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ZScreenLib")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCompany("ZScreen")]
+[assembly: AssemblyProduct("ZScreen")]
+[assembly: AssemblyCopyright("Copyright © ZScreen Developers 2006-2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.1037")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
