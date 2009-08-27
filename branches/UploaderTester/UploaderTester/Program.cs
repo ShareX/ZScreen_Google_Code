1078a5518cda8c31143647d7ac1d8bf1835cac86
flexy123 <flexy123@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-08-27 11:05:46 +0000

    Added Starksoft.Net.Ftp.dll & Starksoft.Net.Proxy.dll to project, FTPUploader progress event support more info (Percentage, estimated complete time etc.)

--- branches/UploaderTester/UploaderTester/Program.cs
+++ branches/UploaderTester/UploaderTester/Program.cs
@@ -3,35 +3,29 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using UploadersLib;
-using UploadersLib.FileUploaders;
-using UploadersLib.ImageUploaders;
+using ZSS;
+using UploadersLib.FileUploaders.FTP;
 
 namespace UploaderTester
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            FlickrUploader flickr = new FlickrUploader("72157621991839563-23481544f632f6b60");
+            FTPAccount account = new FTPAccount();
 
-            FlickrUploader.AuthInfo auth = flickr.CheckToken(flickr.Token);
-            flickr.UserID = auth.UserID;
-
-            flickr.Settings.Title = "Title test";
-            flickr.Settings.Description = "Description test";
-            flickr.Settings.Tags = "Tag1,Tag2,Tag3";
-            flickr.Settings.IsPublic = "0";
-            flickr.Settings.IsFriend = "1";
-            flickr.Settings.IsFamily = "1";
-            flickr.Settings.SafetyLevel = "1";
-            flickr.Settings.ContentType = "2";
-            flickr.Settings.Hidden = "2";
-
-            string url = flickr.UploadImage(@"C:\Users\PC\Desktop\main.png").GetFullImageUrl();
-            Console.WriteLine(url);
+            using (FTPUploader ftp = new FTPUploader(account))
+            {
+                //ftp.ProgressChanged += new FTPUploader.FTPProgressEventHandler(ftp_ProgressChanged);
+                ftp.UploadFile(@"C:\Users\PC\Desktop\xchat-2.8.7f.exe", "UploadTest.exe");
+            }
 
             Console.ReadLine();
+        }
+
+        private static void ftp_ProgressChanged(float percentage)
+        {
+            Console.WriteLine(percentage + "%");
         }
     }
 }
