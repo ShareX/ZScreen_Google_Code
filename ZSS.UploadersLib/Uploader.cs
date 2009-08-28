003203d5d5ffdbed09578c8086ef68be96919d2e
McoreD <mcored@gmail.com>
2009-08-28 13:49:04 +0000

    rearranged checkboxes in options - general; reverted back to supporting default web proxy - http://www.vbforums.com/showthread.php?t=582151

--- ZSS.UploadersLib/Uploader.cs
+++ ZSS.UploadersLib/Uploader.cs
@@ -43,7 +43,7 @@ namespace UploadersLib
 
         public List<string> Errors { get; set; }
         [XmlIgnore]
-        public static WebProxy ProxySettings { get; set; }
+        public static IWebProxy ProxySettings { get; set; }
         public string UserAgent { get; set; }
 
         public Uploader()
