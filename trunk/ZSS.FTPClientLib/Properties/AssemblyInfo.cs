7e95186db5c88fcfaf37907d8063c7360ad63e64
flexy123 <flexy123@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-08-31 09:47:34 +0000

    FTP Client changes

--- trunk/ZSS.FTPClientLib/Properties/AssemblyInfo.cs
+++ trunk/ZSS.FTPClientLib/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FTP")]
+[assembly: AssemblyTitle("ZScreen FTP Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FTP")]
-[assembly: AssemblyCopyright("Copyright ©")]
+[assembly: AssemblyProduct("ZScreen FTP Client")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
