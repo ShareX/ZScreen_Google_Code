d6fcf11a50455b653534dbec418283195d205893
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-09-02 08:25:03 +0000

    svn cleanup and assembly info update

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.4.1.0")]
+[assembly: AssemblyFileVersion("3.4.4.0")]
