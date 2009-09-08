63120e1b36e6f66e4c3c69dec92c330a5f094450
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-09-08 03:41:20 +0000

    implemented posting to Twitter; URL will be appended into the Twitter message

--- trunk/ZSS.UploadersLib/Forms/TwitterMsg.Designer.cs
+++ trunk/ZSS.UploadersLib/Forms/TwitterMsg.Designer.cs
@@ -31,46 +31,62 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TwitterMsg));
             this.btnOK = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
-            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.txtTweet = new System.Windows.Forms.TextBox();
+            this.lblCount = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnOK
             // 
-            this.btnOK.Location = new System.Drawing.Point(120, 96);
+            this.btnOK.Location = new System.Drawing.Point(296, 136);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(81, 24);
             this.btnOK.TabIndex = 0;
-            this.btnOK.Text = "OK";
+            this.btnOK.Text = "&Tweet It";
             this.btnOK.UseVisualStyleBackColor = true;
             this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(208, 96);
+            this.btnCancel.Location = new System.Drawing.Point(384, 136);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(80, 24);
             this.btnCancel.TabIndex = 1;
-            this.btnCancel.Text = "Cancel";
+            this.btnCancel.Text = "&Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
-            // txtMsg
+            // txtTweet
             // 
-            this.txtMsg.Location = new System.Drawing.Point(8, 8);
-            this.txtMsg.Multiline = true;
-            this.txtMsg.Name = "txtMsg";
-            this.txtMsg.Size = new System.Drawing.Size(280, 80);
-            this.txtMsg.TabIndex = 2;
+            this.txtTweet.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTweet.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold);
+            this.txtTweet.Location = new System.Drawing.Point(8, 8);
+            this.txtTweet.Multiline = true;
+            this.txtTweet.Name = "txtTweet";
+            this.txtTweet.Size = new System.Drawing.Size(461, 120);
+            this.txtTweet.TabIndex = 2;
+            this.txtTweet.TextChanged += new System.EventHandler(this.txtTweet_TextChanged);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(8, 144);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(25, 13);
+            this.lblCount.TabIndex = 3;
+            this.lblCount.Text = "140";
             // 
             // TwitterMsg
             // 
             this.AcceptButton = this.btnOK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(296, 128);
-            this.Controls.Add(this.txtMsg);
+            this.ClientSize = new System.Drawing.Size(477, 168);
+            this.Controls.Add(this.lblCount);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtTweet);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -89,6 +105,7 @@
 
         private System.Windows.Forms.Button btnOK;
         private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.TextBox txtMsg;
+        public System.Windows.Forms.TextBox txtTweet;
+        private System.Windows.Forms.Label lblCount;
     }
 }
