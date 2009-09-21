8cf05009e08f832026aaee7817bc2a5e16ac69b2
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2009-09-21 10:02:00 +0000

    gradient maker groupbox moved; keyboard hook is reinitialized after a job is completed

--- trunk/ZScreenLib/Properties/AssemblyInfo.cs
+++ trunk/ZScreenLib/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.1293")]
+[assembly: AssemblyVersion("3.0.0.1306")]
 [assembly: AssemblyFileVersion("3.11.0.1")]
