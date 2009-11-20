6d9b1cc313244b65b4933d6b9191b2a5a91d83a2
McoreD <mcored@gmail.com>
2009-11-20 08:11:39 +0000

    tip for those who dont believe me http://www.moserware.com/2008/01/constants-on-left-are-better-but-this.html; change log update; ready for release of 3.20.5.1

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.20.5.0")]
+[assembly: AssemblyFileVersion("3.20.5.1")]
