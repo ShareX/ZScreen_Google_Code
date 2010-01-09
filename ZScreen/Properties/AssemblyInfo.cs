5190ea1be32ba983fa69307f50e07ad7c2727467
McoreD <mcored@gmail.com>
2010-01-09 11:01:25 +0000

    3.25.0.1 Portable mode will not create a temp folder in %localappdata% anymore [Issue 198]

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.25.0.0")]
+[assembly: AssemblyFileVersion("3.25.0.1")]
