8890b2914fc7afecd64b74ca53422678633dea22
McoreD <mcored@gmail.com>
2010-01-11 11:24:50 +0000

    3.25.0.2 Error while relocating Root folder in Config Wizard under Windows XP [Issue 199]

--- ZScreenLib/Properties/AssemblyInfo.cs
+++ ZScreenLib/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.1538")]
-[assembly: AssemblyFileVersion("3.25.0.1")]
+[assembly: AssemblyVersion("3.0.0.1540")]
+[assembly: AssemblyFileVersion("3.25.0.2")]
