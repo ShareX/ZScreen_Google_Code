58ddc97cf55c042b03c39408e22aa1e50b92310f
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2010-01-14 21:54:20 +0000

    last commit didnt compile

--- trunk/ZScreenLib/Properties/AssemblyInfo.cs
+++ trunk/ZScreenLib/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.1546")]
+[assembly: AssemblyVersion("3.0.0.1548")]
 [assembly: AssemblyFileVersion("3.25.0.2")]
