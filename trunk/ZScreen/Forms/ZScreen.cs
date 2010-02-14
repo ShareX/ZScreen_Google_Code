3150637881cfbaa492f06312f2b2a32526b131c2
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2010-02-14 01:32:13 +0000

    changes as per issue 219

--- trunk/ZScreen/Forms/ZScreen.cs
+++ trunk/ZScreen/Forms/ZScreen.cs
@@ -162,7 +162,7 @@ namespace ZScreenGUI
         {
             this.Icon = Resources.zss_main;
             this.Text = Engine.GetProductName();
-            this.niTray.Text = this.Text;
+            this.niTray.Text = (this.Text + " - " + Application.ExecutablePath).Substring(0, 60) + "...";
             this.lblLogo.Text = this.Text;
 
             if (this.WindowState == FormWindowState.Normal)
