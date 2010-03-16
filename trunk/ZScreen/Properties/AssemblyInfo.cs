b022a173c2a2a944717dd86b335bd7e7701b3020
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2010-03-16 04:06:54 +0000

    3.27.3.1 release

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.27.3.0")]
+[assembly: AssemblyFileVersion("3.27.3.1")]
