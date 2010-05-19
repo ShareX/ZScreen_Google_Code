1ebef753a736301cbe3745d95da982549874d33b
McoreD <mcored@gmail.com>
2010-05-19 11:02:41 +0000

    fix xml setting save error

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.28.3.2")]
+[assembly: AssemblyFileVersion("3.30.0.0")]
