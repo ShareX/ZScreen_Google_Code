6b102a6f28e89e5307342473f4f947fab6a872ea
Jaex <Jaex@users.noreply.github.com>
2010-11-18 19:33:55 +0000

    Solution changes

--- ZScreenLib/Properties/AssemblyInfo.cs
+++ ZScreenLib/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.1700")]
+[assembly: AssemblyVersion("3.0.0.1716")]
 [assembly: AssemblyFileVersion("3.32.0.1")]
