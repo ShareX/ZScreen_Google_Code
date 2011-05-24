8cb5fb8f33c1f47f28964a7b8949289d6ec12ceb
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-05-24 09:09:31 +0000

    gui fixes

--- trunk/ZScreenLib/Properties/AssemblyInfo.cs
+++ trunk/ZScreenLib/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.2165")]
+[assembly: AssemblyVersion("3.0.0.2173")]
 [assembly: AssemblyFileVersion("3.36.17.0")]
