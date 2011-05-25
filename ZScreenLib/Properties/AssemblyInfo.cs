9420004d54968456e1ba2690c37b3a12d5806757
McoreD <mcored@gmail.com>
2011-05-25 10:42:06 +0000

    ZScreen.exe Assembly Version will now include the Google Code revision number in its Revision

--- ZScreenLib/Properties/AssemblyInfo.cs
+++ ZScreenLib/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.0.0.2177")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("4.0.1.2181")]
+[assembly: AssemblyFileVersion("4.0.1")]
