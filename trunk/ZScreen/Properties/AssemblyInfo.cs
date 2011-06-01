6dd6b917628478e97d11ba5317867e482eaa0728
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-06-01 05:20:11 +0000

    released http://zscreen.googlecode.com/files/ZScreen-4.0.1.2218-setup.exe via updates

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("4.0.1.2218")]
+[assembly: AssemblyVersion("4.0.1.2219")]
 [assembly: AssemblyFileVersion("4.0.1")]
