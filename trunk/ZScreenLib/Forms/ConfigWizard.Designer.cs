569d876db2b4c895b88b33c7416a2c5bb53e2654
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-06-22 01:44:14 +0000

    dev version 2256 ready; configwizard backcolor changed to white

--- trunk/ZScreenLib/Forms/ConfigWizard.Designer.cs
+++ trunk/ZScreenLib/Forms/ConfigWizard.Designer.cs
@@ -150,6 +150,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(625, 318);
             this.Controls.Add(this.ucDestOptions);
             this.Controls.Add(this.chkPreferSystemFolders);
