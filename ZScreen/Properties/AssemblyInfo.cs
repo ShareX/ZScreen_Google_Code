11d41e9c6170d4e1ea79c381a5df5f2065ff309f
McoreD <mcored@gmail.com>
2011-07-24 00:27:32 +0000

    released rev 2360 as dev

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("4.2.1.2357")]
+[assembly: AssemblyVersion("4.2.1.2360")]
 [assembly: AssemblyFileVersion("4.2.1")]
