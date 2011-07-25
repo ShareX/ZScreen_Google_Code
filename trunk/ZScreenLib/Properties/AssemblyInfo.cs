ddd70aaa19729893073094f90ae04e07f6d206c4
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-07-25 03:23:24 +0000

    ffs "File" as output was causing null error

--- trunk/ZScreenLib/Properties/AssemblyInfo.cs
+++ trunk/ZScreenLib/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.2.1.2360")]
+[assembly: AssemblyVersion("4.2.1.2365")]
 [assembly: AssemblyFileVersion("4.2.1")]
