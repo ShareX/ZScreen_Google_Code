b121dd3b327c5d8d2eee0c50f608a98840a7fc80
McoreD <mcored@gmail.com>
2011-08-07 06:58:48 +0000

    Thanks to Greenshot folks, we have incorporated Greenshot Image Editor. OSS FTW.

--- ZScreenLib/Properties/AssemblyInfo.cs
+++ ZScreenLib/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.2.2.2381")]
-[assembly: AssemblyFileVersion("4.2.2")]
+[assembly: AssemblyVersion("4.2.3.2381")]
+[assembly: AssemblyFileVersion("4.2.3")]
