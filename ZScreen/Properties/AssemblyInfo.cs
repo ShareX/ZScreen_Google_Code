ca69cffbce5351377c292684713a59c07ff3e984
McoreD <mcored@gmail.com>
2011-08-22 02:39:42 +0000

    clicking balloonTip did not open url

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("4.2.4.2426")]
+[assembly: AssemblyVersion("4.2.4.2435")]
 [assembly: AssemblyFileVersion("4.2.4")]
