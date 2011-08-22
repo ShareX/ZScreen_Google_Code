c74b59db613218a54bd1f7e274f3fe4b5751a04b
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-08-22 11:37:29 +0000

    ZScreenLite (ZSL) version history update

--- trunk/ZUploader/Properties/AssemblyInfo.cs
+++ trunk/ZUploader/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ZUploader")]
+[assembly: AssemblyTitle("ZScreenLite")]
 [assembly: AssemblyDescription("A program that allows you to upload images, texts or files")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ZScreen Developers")]
-[assembly: AssemblyProduct("ZUploader")]
+[assembly: AssemblyProduct("ZScreenLite")]
 [assembly: AssemblyCopyright("Copyright 2008-2011 ZScreen Developers")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.12.0.0")]
-[assembly: AssemblyFileVersion("3.12.0.0")]
+[assembly: AssemblyVersion("4.0.0.2438")]
+[assembly: AssemblyFileVersion("4.0.0")]
--- trunk/ZUploaderPlugin/Properties/AssemblyInfo.cs
+++ trunk/ZUploaderPlugin/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ZUploaderPlugin")]
+[assembly: AssemblyTitle("ZScreenLitePlugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ZUploaderPlugin")]
+[assembly: AssemblyProduct("ZScreenLitePlugin")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
