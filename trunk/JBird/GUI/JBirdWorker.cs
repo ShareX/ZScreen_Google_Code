eec678bd4570a22b642d2985ca7d4ef56f00cb56
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-08-31 14:00:24 +0000

    missing JBird items

---
+++ trunk/JBird/GUI/JBirdWorker.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ZScreenLib;
+
+namespace JBirdGUI
+{
+    public partial class JBirdMain : Form
+    {
+
+        public BackgroundWorker CreateWorker()
+        {
+            BackgroundWorker bwApp = new BackgroundWorker { WorkerReportsProgress = true };
+            bwApp.DoWork += new System.ComponentModel.DoWorkEventHandler(BwApp_DoWork);
+            bwApp.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(BwApp_ProgressChanged);
+            bwApp.RunWorkerCompleted += new RunWorkerCompletedEventHandler(BwApp_RunWorkerCompleted);
+            return bwApp;
+        }
+
+        public void BwApp_DoWork(object sender, DoWorkEventArgs e)
+        {
+            WorkerTask bwTask = e.Argument as WorkerTask;
+            
+            // process outputs e.g. upload
+
+            e.Result = bwTask;
+        }
+
+        private void BwApp_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // update gui
+        }
+
+        private void BwApp_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            WorkerTask bwTask = e.Result as WorkerTask;
+
+            UploadManager.ShowUploadResults(bwTask, true);
+        }
+
+        public WorkerTask CreateTask(Profile profile)
+        {
+            WorkerTask tempTask = new WorkerTask(CreateWorker(), profile);
+            return tempTask;
+        }
+
+        public void CaptureEntireScreen()
+        {
+            WorkerTask esTask = CreateTask(Program.GetProfile(WorkerTask.JobLevel2.CaptureEntireScreen));
+            esTask.WasToTakeScreenshot = true;
+            esTask.RunWorker();
+        }
+
+        public void CaptureActiveWindow()
+        {
+            
+        }
+
+    }
+}
