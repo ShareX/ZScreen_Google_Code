e0b472e67a6d636407f456daa37eedca1fd29f29
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-08-31 03:50:21 +0000

    released dev build r2476

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("4.2.5.2470")]
+[assembly: AssemblyVersion("4.2.5.2476")]
 [assembly: AssemblyFileVersion("4.2.5")]
--- trunk/ZScreenLib/Properties/AssemblyInfo.cs
+++ trunk/ZScreenLib/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.2.5.2470")]
+[assembly: AssemblyVersion("4.2.5.2476")]
 [assembly: AssemblyFileVersion("4.2.5")]
