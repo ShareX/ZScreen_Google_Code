4a57bd00a642ab6e5d6f752cc12d2f84818e158a
flexy123 <flexy123@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-09-05 21:44:07 +0000

    ScreenCapture folder commit

---
+++ trunk/ScreenCapture/Enums.cs
@@ -0,0 +1,6 @@
+﻿namespace ScreenCapture
+{
+    public enum NodePosition { TopLeft, Top, TopRight, Right, BottomRight, Bottom, BottomLeft, Left }
+
+    public enum TriangleAngle { Top, Right, Bottom, Left }
+}
