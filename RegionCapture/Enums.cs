7e2ffebc0fbe57774be7bffaa06d9b33f477aa19
Jaex <Jaex@users.noreply.github.com>
2011-09-05 21:39:04 +0000

    RegionCapture name changed

--- RegionCapture/Enums.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ScreenCapture
-{
-    public enum NodePosition { TopLeft, Top, TopRight, Right, BottomRight, Bottom, BottomLeft, Left }
-
-    public enum TriangleAngle { Top, Right, Bottom, Left }
-}
