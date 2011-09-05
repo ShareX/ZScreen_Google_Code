df08d312b62bc63ca7c826567eb3d130f55de0d7
flexy123 <flexy123@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-09-05 01:11:05 +0000

    Added hotkey support to ZUploader plugins, Added default hotkeys for screenshot plugin

--- trunk/RegionCapture/NativeMethods.cs
+++ trunk/RegionCapture/NativeMethods.cs
@@ -29,9 +29,9 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 
-namespace HelpersLib
+namespace RegionCapture
 {
-    internal static class NativeMethods
+    public static class NativeMethods
     {
         [DllImport("user32.dll")]
         public static extern IntPtr GetForegroundWindow();
@@ -179,59 +179,59 @@ namespace HelpersLib
 
             return windowRect;
         }
-    }
 
-    [StructLayout(LayoutKind.Sequential)]
-    public struct RECT
-    {
-        public int Left;
-        public int Top;
-        public int Right;
-        public int Bottom;
-
-        public RECT(int left_, int top_, int right_, int bottom_)
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
         {
-            Left = left_;
-            Top = top_;
-            Right = right_;
-            Bottom = bottom_;
-        }
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
 
-        public int Height { get { return Bottom - Top; } }
-        public int Width { get { return Right - Left; } }
+            public RECT(int left_, int top_, int right_, int bottom_)
+            {
+                Left = left_;
+                Top = top_;
+                Right = right_;
+                Bottom = bottom_;
+            }
 
-        public Size Size { get { return new Size(Width, Height); } }
-        public Point Location { get { return new Point(Left, Top); } }
+            public int Height { get { return Bottom - Top; } }
+            public int Width { get { return Right - Left; } }
 
-        public Rectangle ToRectangle()
-        {
-            return Rectangle.FromLTRB(Left, Top, Right, Bottom);
-        }
+            public Size Size { get { return new Size(Width, Height); } }
+            public Point Location { get { return new Point(Left, Top); } }
 
-        public static RECT FromRectangle(Rectangle rectangle)
-        {
-            return new RECT(rectangle.Left, rectangle.Top, rectangle.Right, rectangle.Bottom);
-        }
+            public Rectangle ToRectangle()
+            {
+                return Rectangle.FromLTRB(Left, Top, Right, Bottom);
+            }
 
-        public override int GetHashCode()
-        {
-            return Left ^ ((Top << 13) | (Top >> 0x13))
+            public static RECT FromRectangle(Rectangle rectangle)
+            {
+                return new RECT(rectangle.Left, rectangle.Top, rectangle.Right, rectangle.Bottom);
+            }
+
+            public override int GetHashCode()
+            {
+                return Left ^ ((Top << 13) | (Top >> 0x13))
               ^ ((Width << 0x1a) | (Width >> 6))
               ^ ((Height << 7) | (Height >> 0x19));
+            }
+
+            #region Operator overloads
+
+            public static implicit operator Rectangle(RECT rect)
+            {
+                return rect.ToRectangle();
+            }
+
+            public static implicit operator RECT(Rectangle rect)
+            {
+                return FromRectangle(rect);
+            }
+
+            #endregion Operator overloads
         }
-
-        #region Operator overloads
-
-        public static implicit operator Rectangle(RECT rect)
-        {
-            return rect.ToRectangle();
-        }
-
-        public static implicit operator RECT(Rectangle rect)
-        {
-            return FromRectangle(rect);
-        }
-
-        #endregion Operator overloads
     }
 }
