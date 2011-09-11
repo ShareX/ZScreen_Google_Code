e38aecb3ffd68742ad4921c36dd0f069d1ba4490
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-09-11 10:07:50 +0000

    UI support for the feature 4.03.6 Goo.gl URL Shortener now supports User Accounts

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("4.2.5.2520")]
-[assembly: AssemblyFileVersion("4.2.5")]
+[assembly: AssemblyVersion("4.2.6.2520")]
+[assembly: AssemblyFileVersion("4.2.6")]
