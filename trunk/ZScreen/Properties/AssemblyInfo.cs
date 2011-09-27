1e38cb0e72db8229c2c8c248f67c916cd2b94a99
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-09-27 02:53:02 +0000

    if (tempTask.IsNotCanceled())                 tempTask.SetNotifyIconStatus(this.niTray, Resources.zss_busy);

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("4.4.1.2614")]
+[assembly: AssemblyVersion("4.4.1.2617")]
 [assembly: AssemblyFileVersion("4.4.1")]
