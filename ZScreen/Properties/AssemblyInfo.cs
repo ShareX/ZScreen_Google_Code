ee9772c07e5ad8398b9e9c444e6c6c65bed43220
McoreD <mcored@gmail.com>
2011-09-29 02:47:15 +0000

    updated version history to include 4.4.2 Added support to use a custom sound after the task is finished via Advanced > Settings {TheDeathly}

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("4.4.1.2629")]
-[assembly: AssemblyFileVersion("4.4.1")]
+[assembly: AssemblyVersion("4.4.2.2632")]
+[assembly: AssemblyFileVersion("4.4.2")]
