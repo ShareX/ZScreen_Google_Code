a26ed6b5fcc41b4bbca15eb73314d1fdb19992c9
McoreD <mcored@gmail.com>
2011-10-02 13:00:23 +0000

    hotkey errors will now only display once

--- ZSS.HelpersLib/Hotkey/HotkeyInfo.cs
+++ ZSS.HelpersLib/Hotkey/HotkeyInfo.cs
@@ -31,12 +31,10 @@ namespace HelpersLib
     public class HotkeyInfo
     {
         public ushort ID { get; private set; }
-
         public Keys Key { get; private set; }
-
         public Action HotkeyPress { get; set; }
-
         public int Tag { get; set; }
+        public string Error { get; set; }
 
         public HotkeyInfo(ushort id, Keys key, Action hotkeyPress = null, int tag = -1)
         {
