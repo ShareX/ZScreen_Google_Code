729a4c022af12f1b566cbdc9330b1b68e27dfac1
McoreD <mcored@gmail.com>
2011-10-05 08:45:10 +0000

    Capture Shape in ZScreen now acknowledges DrawCursor setting

--- ZScreenLib/Properties/AssemblyInfo.cs
+++ ZScreenLib/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.5.1.2687")]
+[assembly: AssemblyVersion("4.5.1.2690")]
 [assembly: AssemblyFileVersion("4.5.1")]
