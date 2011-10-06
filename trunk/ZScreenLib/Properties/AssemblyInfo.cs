fb627c7c285d77b1bd42ced1c6f3c243170c1fe6
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-10-06 12:05:09 +0000

    File Upload - if the images are uploaded from the ZScreen images dir then the images will not be duplicated anymore

--- trunk/ZScreenLib/Properties/AssemblyInfo.cs
+++ trunk/ZScreenLib/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.5.1.2691")]
+[assembly: AssemblyVersion("4.5.1.2697")]
 [assembly: AssemblyFileVersion("4.5.1")]
