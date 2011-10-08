455a3951dc2fbc8508a132c7ff11fe5871de3a2e
TheDeathly@gmail.com <TheDeathly@gmail.com@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-10-08 16:23:52 +0000

    Added in some encryption classes

---
+++ trunk/ZScreenLib/Cryptography/CryptKeys.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Xml;
+
+namespace ZScreenLib.Cryptography
+{
+    class CryptKeys
+    {
+        CspParameters cp;
+        RSACryptoServiceProvider rsa;
+        string Container = string.Empty;
+        public CryptKeys(string container = "ZScreen")
+        {
+            Container = container;
+            cp = new CspParameters();
+            cp.KeyContainerName = Container;
+        }
+
+        public void GenerateKey()
+        {
+            rsa = new RSACryptoServiceProvider(cp);
+        }
+        public String ReturnKeys()
+        {
+            return rsa.ToXmlString(true);
+        }
+        public void DeleteKey()
+        {
+            rsa.PersistKeyInCsp = false;
+            rsa.Clear();
+        }
+        public CryptInfo ReturnInfo()
+        {
+            CryptInfo ci = new CryptInfo();
+            string xml = ReturnKeys();
+            XmlDocument document = new XmlDocument();
+            document.LoadXml(xml);
+            ci.Password = document.DocumentElement.SelectSingleNode("//P").InnerXml;
+            ci.Salt = document.DocumentElement.SelectSingleNode("//Q").InnerText;
+            ci.Vector = document.DocumentElement.SelectSingleNode("//D").InnerText.Substring(0, 16);
+            return ci;
+        }
+        public string Encrypt(string PlainText, CryptInfo ci, int KeySize, string EncryptionMethod = CryptMethod.SHA1, int PassIterations = 2)
+        {
+            return AESEncryption.Encrypt(PlainText, ci.Password, ci.Salt, EncryptionMethod, PassIterations, ci.Vector, KeySize);
+        }
+
+        public string Decrypt(string CryptedText, CryptInfo ci, int Keysize, string EncryptionMethod = CryptMethod.SHA1, int PassIterations = 2)
+        {
+            return AESEncryption.Decrypt(CryptedText, ci.Password, ci.Salt, EncryptionMethod, PassIterations, ci.Vector, Keysize);
+        }
+    }
+}
---
+++ trunk/ZScreenLib/Cryptography/CryptMethod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZScreenLib.Cryptography
+{
+    internal static class CryptMethod
+    {
+        public const string SHA1 = "SHA1";
+        public const string MD5 = "MD5";
+    }
+}
