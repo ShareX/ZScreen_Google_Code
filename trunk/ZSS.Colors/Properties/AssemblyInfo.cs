c9a93c0884928a4a3d8e5c80aa79c92722bfb6a3
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-10-23 13:48:32 +0000

    updated SVNRevToAssemblyInfo.exe to automatically scan all the AssemblyInfo.cs files and update SVN Revision run AssemblyInfoUpdate.cmd to use this feature

--- trunk/ZSS.Colors/Properties/AssemblyInfo.cs
+++ trunk/ZSS.Colors/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿#region License Information (GPL v2)
+#region License Information (GPL v2)
 
 /*
     ZScreen - A program that allows you to upload screenshots in one keystroke.
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.2815")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
