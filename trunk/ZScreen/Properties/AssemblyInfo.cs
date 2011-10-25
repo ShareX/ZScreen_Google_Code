7072e4974f43f2f075ee154a8eee09c4430054ee
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-10-25 11:21:09 +0000

    fixed issue 233 ZScreen 4.7.4 r2832 http://i51.tinypic.com/6dxste.png 71 KiB

--- trunk/ZScreen/Properties/AssemblyInfo.cs
+++ trunk/ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("4.7.3.2832")]
-[assembly: AssemblyFileVersion("4.7.3")]
+[assembly: AssemblyVersion("4.7.4.2832")]
+[assembly: AssemblyFileVersion("4.7.4")]
