70eff95c8034d664c82d41be32257881e3527c6a
mcored <mcored@3d133d20-8598-11dd-90a9-6306d3ffb32f>
2011-11-22 23:35:34 +0000

    released ZScreen r2924 stable

--- branches/4/ZScreen/Properties/AssemblyInfo.cs
+++ branches/4/ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("4.7.5.2905")]
+[assembly: AssemblyVersion("4.7.5.2924")]
 [assembly: AssemblyFileVersion("4.7.5")]
