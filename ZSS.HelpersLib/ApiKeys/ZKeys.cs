488071901f2e132226bfeb7da4202b43f1c7dcf5
McoreD <mcored@gmail.com>
2012-03-15 22:54:20 +0000

    AssemblyInfo.cs files update removed all api keys

--- ZSS.HelpersLib/ApiKeys/ZKeys.cs
+++ ZSS.HelpersLib/ApiKeys/ZKeys.cs
@@ -28,48 +28,48 @@ namespace HelpersLib
     public static class ZKeys
     {
         // Image Uploaders
-        public const string ImageShackKey = "78EHNOPS04e77bc6df1cc0c5fc2e92e11c7b4a1a";
-        public const string TinyPicID = "e2aabb8d555322fa";
-        public const string TinyPicKey = "00a68ed73ddd54da52dc2d5803fa35ee";
-        public const string ImgurAnonymousKey = "af2fde9818ae53e7670ab52fb8ade644"; // "97d0aaf3f70d10cc96d8b8e62431f4d2";
-        public const string ImgurConsumerKey = "89dec1eceb03e7028647445fdde8af2204e87fd58"; // "cc6a3227dc7cbe15d2754b194ae3c26504db122ab";
-        public const string ImgurConsumerSecret = "77e4e25ba8bf9c3ce2a9b9a09092d3d0"; // "edd13f72e7c9908b50c8090a8e912b73";
-        public const string FlickrKey = "009382d913746758f23d0ba9906b9fde";
-        public const string FlickrSecret = "7a147b763b1c7ebc";
-        public const string PhotobucketConsumerKey = "149828681";
-        public const string PhotobucketConsumerSecret = "d2638b653e88315aac528087e9db54e3";
-        public const string UploadScreenshotKey = "2807828f379860848433221358";
-        public const string ImageBamKey = "P9MMWVORXYCVM9LL";
-        public const string ImageBamSecret = "8NHVT2W777DEHZBV54A4CIT23XJFTL1D";
-        public const string TwitsnapsKey = "694aaea3bc8f28479c383cbcc094fb55";
+        public const string ImageShackKey = "";
+        public const string TinyPicID = "";
+        public const string TinyPicKey = "";
+        public const string ImgurAnonymousKey = "";
+        public const string ImgurConsumerKey = "";
+        public const string ImgurConsumerSecret = "";
+        public const string FlickrKey = "";
+        public const string FlickrSecret = "";
+        public const string PhotobucketConsumerKey = "";
+        public const string PhotobucketConsumerSecret = "";
+        public const string UploadScreenshotKey = "";
+        public const string ImageBamKey = "";
+        public const string ImageBamSecret = "";
+        public const string TwitsnapsKey = "";
 
         // File Uploaders
-        public const string DropboxConsumerKey = "0te7j9ype9lrdfn";
-        public const string DropboxConsumerSecret = "r5d3aptd9a0cwp9";
-        public const string MinusConsumerKey = "b57b69843f7a302a276dde89890fc6";
-        public const string MinusConsumerSecret = "3fb097f08314d713959b1f41d543b0";
-        public const string BoxKey = "5vd8549xmn9a7zil4iupb0ciroj7cvee";
-        public const string SendSpaceKey = "LV6OS1R0Q3";
-        public const string DropIOKey = "6c65e2d2bfd858f7d0aa6509784f876483582eea";
+        public const string DropboxConsumerKey = "";
+        public const string DropboxConsumerSecret = "";
+        public const string MinusConsumerKey = "";
+        public const string MinusConsumerSecret = "";
+        public const string BoxKey = "";
+        public const string SendSpaceKey = "";
+        public const string DropIOKey = "";
 
         // Text Uploaders
-        public const string PastebinKey = "4b23be71ec78bbd4fb96735320aa09ef";
-        public const string PastebinCaKey = "KxTofLKQThSBZ63Gpa7hYLlMdyQlMD6u";
+        public const string PastebinKey = "";
+        public const string PastebinCaKey = "";
 
         // URL Shorteners
-        public const string BitlyLogin = "jaex";
-        public const string BitlyKey = "R_1734f57b772acb3c048eb2365075743b";
-        public const string BitlyConsumerKey = "91e5a02e001ada9c1122f54d73bc442d9cc2a7ab";
-        public const string BitlyConsumerSecret = "fe5f906b5f5e8114d5c266a709a9438c94e1cd3f";
-        public const string GoogleConsumerKey = "1011702346808.apps.googleusercontent.com";
-        public const string GoogleConsumerSecret = "EmT1pAOddpDRYUDd5smypTbH";
-        public const string KlamKey = "a4e5a8de710d80db774a8264f4588ffb";
-        public const string ThreelyKey = "em5893833";
+        public const string BitlyLogin = "";
+        public const string BitlyKey = "";
+        public const string BitlyConsumerKey = "";
+        public const string BitlyConsumerSecret = "";
+        public const string GoogleConsumerKey = "";
+        public const string GoogleConsumerSecret = "";
+        public const string KlamKey = "";
+        public const string ThreelyKey = "";
 
         // Other Services
-        public const string TwitterConsumerKey = "Jzzcm6ytcyml14sQIvqvmA";
-        public const string TwitterConsumerSecret = "aJYZ9W1gJnGMgSqhRYrvoUyUc14FssVJOFAqHjriU";
-        public const string GoogleApiKey = "AIzaSyCcYJvYPvS3UE0JqqsSNpjPjN1NPBmMbmE";
-        public const string PicnikKey = "3aacd2de4563b8817de708b29b5bdd0e";
+        public const string TwitterConsumerKey = "";
+        public const string TwitterConsumerSecret = "";
+        public const string GoogleApiKey = "";
+        public const string PicnikKey = "";
     }
 }
