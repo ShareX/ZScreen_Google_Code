5b3548176d01f511dd980068bc12ef0a13286711
McoreD <mcored@gmail.com>
2012-04-22 02:21:59 +0000

    released http://zscreen.googlecode.com/files/ZScreen-4.9.0.3051-dev-setup.exe

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("4.9.0.3046")]
+[assembly: AssemblyVersion("4.9.0.3051")]
 [assembly: AssemblyFileVersion("4.9.0")]
