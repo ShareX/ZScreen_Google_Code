a677c3c713bfb61f9b2a6af89992d2afb0bc9b28
McoreD <mcored@gmail.com>
2012-05-13 06:02:28 +0000

    released ZScreen r3057 fixed bug: copying same file to a Shared Folder would cause ZScreen crash fixed bug: update check prompted even after turning off the checkBox

--- ZScreen/Properties/AssemblyInfo.cs
+++ ZScreen/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("4.9.0.3051")]
+[assembly: AssemblyVersion("4.9.0.3057")]
 [assembly: AssemblyFileVersion("4.9.0")]
